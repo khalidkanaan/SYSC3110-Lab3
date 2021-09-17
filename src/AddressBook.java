f4fece9bfbb7176a983c8bee06a4bad127a17834
Khalid <kfkanaan@gmail.com>
2021-09-17 01:26:35 -0400

    Changed the main method to add & remove a buddy from an address book.

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,6 +1,25 @@
+import java.util.ArrayList;
+
 public class AddressBook {
 
+    private static ArrayList<BuddyInfo> buddies;
+
+    public AddressBook(){
+        buddies = new ArrayList<BuddyInfo>();
+    }
+
+    public void addBuddy(BuddyInfo buddy) {
+        buddies.add(buddy);
+    }
+
+    public void removeBuddy(BuddyInfo buddy) {
+        buddies.remove(buddy);
+    }
+
     public static void main(String args[]){
-        System.out.println("Address Book");
+        BuddyInfo khalid = new BuddyInfo("Khalid", "Bank St.", "613");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(khalid);
+        addressBook.removeBuddy(khalid);
     }
 }
