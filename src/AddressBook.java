6da0e97bfb8797ca495ed5ece78186c66a19da86
Khalid <kfkanaan@gmail.com>
2021-09-24 00:36:08 -0400

    Improved addBuddy and removeBuddy functions to include if statements

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -9,17 +9,22 @@ public class AddressBook {
     }
 
     public void addBuddy(BuddyInfo buddy) {
-        buddies.add(buddy);
+        if(buddy != null) {
+            buddies.add(buddy);
+        }
     }
 
-    public void removeBuddy(BuddyInfo buddy) {
-        buddies.remove(buddy);
+    public BuddyInfo removeBuddy(int index) {
+        if(index >= 0 && index < buddies.size()){
+            return buddies.remove(index);
+        }
+        return null;
     }
 
     public static void main(String args[]){
         BuddyInfo khalid = new BuddyInfo("Khalid", "Bank St.", "613");
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(khalid);
-        addressBook.removeBuddy(khalid);
+        addressBook.removeBuddy(0);
     }
 }
